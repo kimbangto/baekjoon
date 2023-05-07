061eac1dcf2c24e7193a5e84906163552cda137e
kimbangto <129056529+kimbangto@users.noreply.github.com>
2023-05-07 10:14:17 +0900

    [level 2] Title: 예상 대진표, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
@@ -0,0 +1,12 @@
+function solution(n,a,b)
+{
+    let answer = 1;
+
+while (true) {
+  if (Math.ceil(a / 2) === Math.ceil(b / 2)) return answer;
+  a = Math.ceil(a / 2);
+  b = Math.ceil(b / 2);
+  answer++;
+}
+
+}
