8a62ed23b6da6e57484994c9b30ffb45706fa7e3
kimbangto <129056529+kimbangto@users.noreply.github.com>
2023-05-21 22:57:49 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.05 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,15 @@
+function solution(n)
+{
+    let answer = 0;
+
+    while (n !== 0) {
+      if (n % 2 !== 0) {
+        answer++;
+        n--;
+      } else {
+        n /= 2;
+      }
+    }
+
+    return answer;
+}
