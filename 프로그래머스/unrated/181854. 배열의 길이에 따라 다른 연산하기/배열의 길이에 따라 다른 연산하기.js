f0695c9a290d4e206060ca2b601f84509510c76a
kimbangto <129056529+kimbangto@users.noreply.github.com>
2023-06-15 17:31:55 +0900

    [unrated] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.js
@@ -0,0 +1,15 @@
+function solution(arr, n) {
+    const answer = [];
+    if(arr.length % 2 !== 0) {
+        for(let i=0; i<arr.length; i++) {
+            if(i % 2 === 0) answer.push(arr[i] + n);
+            else answer.push(arr[i]);
+        }
+    } else {
+        for(let i=0; i<arr.length; i++) {
+            if(i % 2 !== 0) answer.push(arr[i] + n);
+            else answer.push(arr[i]);
+        }
+    }
+    return answer;
+}
