b84e0b667f4c059bca2c6bb1bfc6962a101533a2
kimbangto <129056529+kimbangto@users.noreply.github.com>
2024-07-26 01:02:47 +0900

    [level 2] Title: 타겟 넘버, Time: 43.12 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,18 +1,20 @@
 function solution(numbers, target) {
-  let answer = 0;
-
-  const dfs = (index, sum) => {
-    if (index === numbers.length) {
-      if (sum === target) answer++;
-
-      return;
+    let answer = 0;
+    
+    const dfs = (index, sum) => {
+        if(index === numbers.length) {
+            if(sum === target) {
+                answer++;
+                return;
+            }
+            return;
+        }
+        
+        dfs(index + 1, sum + numbers[index]);
+        dfs(index + 1, sum - numbers[index]);
     }
-
-    dfs(index + 1, sum + numbers[index]);
-    dfs(index + 1, sum - numbers[index]);
-  };
-
-  dfs(0, 0);
-
-  return answer;
+    
+    dfs(0, 0);
+    
+    return answer;
 }
