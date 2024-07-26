acf0af22c5452fe738c1995082df54ad4e801b7a
kimbangto <129056529+kimbangto@users.noreply.github.com>
2024-07-26 14:14:36 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 33.96 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,46 +1,42 @@
 function solution(maps) {
+
   const n = maps.length;
   const m = maps[0].length;
 
   const directions = [
-    [0, 1], // 동
-    [0, -1], // 서
-    [1, 0], // 남
-    [-1, 0], // 북
+    [0, 1],
+    [0, -1],
+    [1, 0],
+    [-1, 0],
   ];
 
+  const visited = Array.from(Array(n), () => Array.from(Array(m)).fill(false));
+  visited[0][0] = true;
+
   const bfs = () => {
-    const queue = [[0, 0, 1]]; // 시작점, 거리([0,0]에서 시작, 1칸)
-    const visited = Array.from(Array(n), () => Array(m).fill(false));
-    visited[0][0] = true; // 시작지점 방문경로에 추가
+    const queue = [[0, 0, 1]]; // 시작좌표, 움직인거리
 
-    while (queue.length) {
-      const [x, y, distance] = queue.shift();
+    while (0 < queue.length) {
+      let here = queue.shift();
 
-      if (x === n - 1 && y === m - 1) {
-        // 목표지점 도달
-        return distance;
+      if (here[0] === n - 1 && here[1] === m - 1) {
+        return here[2];
       }
 
-      for (const [dx, dy] of directions) {
-        const nx = x + dx; // nx, ny =>
-        const ny = y + dy; // 동or서or남or북 으로 한칸 움직인 좌표
+      let [oldN, oldM, move] = here;
 
-        if (
-          0 <= nx &&
-          nx < n && //
-          0 <= ny &&
-          ny < m && // 지도 범위(0 ~ n or m) 안쪽
-          maps[nx][ny] === 1 && // 막혀있지 않은 길
-          !visited[nx][ny] // 가보지 않은 길
-        ) {
-          queue.push([nx, ny, distance + 1]);
-          visited[nx][ny] = true;
+      directions.forEach((direction) => {
+        newN = oldN + direction[0];
+        newM = oldM + direction[1];
+
+        if (0 <= newN && newN < n && 0 <= newM && newM < m && maps[newN][newM] === 1 && !visited[newN][newM]) {
+          queue.push([newN, newM, move + 1]);
+          visited[newN][newM] = true;
         }
-      }
+      });
     }
 
-    return -1; // 목표지점 도달 불가능
+    return -1;
   };
 
   return bfs();
