6c41e3ff1d3c9984c5d715136bdd7d2ccda4cd83
kimbangto <129056529+kimbangto@users.noreply.github.com>
2025-09-11 09:47:14 +0900

    [Bronze V] Title: 사칙연산, Time: 100 ms, Memory: 14524 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val input = readln()
+    val split = input.split(" ")
+    val a = split[0].toInt()
+    val b = split[1].toInt()
+    println(a+b)
+    println(a-b)
+    println(a*b)
+    println(a/b)
+    println(a%b)
+}
